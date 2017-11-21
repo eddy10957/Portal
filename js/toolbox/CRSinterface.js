254887ea918657917229620fda345a3823f01b23
eddy10957 <eddytd96@gmail.com>
2017-11-21 17:01:49 +0100

    levate parentesi

--- js/toolbox/CRSinterface.js
+++ js/toolbox/CRSinterface.js
@@ -13,9 +13,9 @@ function CRSinterface_numtochar(){
     $("#output-numtochar").html(result);            // Renderizza il risultato nell'area di output
 }
 function CRSinterface_cesarcipher(){
-  var input01 = $("#input01-cesare".val());
-  var input02 = $("#input02-cesare".val());
-  var input03 = $("#input03-cesare".val());
+  var input01 = $("#input01-cesare".val();
+  var input02 = $("#input02-cesare".val();
+  var input03 = $("#input03-cesare".val();
   var result  = CRStoolbox_cesarcipher(input01,input02,input03);
   $("#output-cesare").html(result);
 }
